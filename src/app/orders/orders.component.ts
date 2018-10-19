4b686e9010e4aa33765d31e1a86e0ac78e78016e
josephraguilar <josephraguilar@gmail.com>
2018-10-18 17:38:39 -0700

    exam end

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { FlexModalService } from '../shared-components/flex-modal/flex-modal.service';
 import { Http } from '@angular/http';
 
+export interface IOrder {pid?: string, image?: string, description?: string, price?: number, quantity?: number}
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -11,7 +13,10 @@ import { Http } from '@angular/http';
 
 export class OrdersComponent implements OnInit {
 
-  orders: Array<any> = [];
+  orders: Array<IOrder> = [];
+  errorMessage:string = '';
+  name:string = '';
+  confirmMessage:string = '';
 
   constructor(
     private router: Router,
@@ -21,6 +26,107 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
+
+  }
+
+  displayOrder() {
+    this.orders = [{
+      "pid": "1",
+      "image":"assets/sm_android.jpeg",
+      "description": "Android",
+      "price": 150.00,
+      "quantity": 2
+    }, {
+      "pid": "2",
+      "image":"assets/sm_iphone.jpeg",
+      "description": "IPhone",
+      "price": 200.00,
+      "quantity": 1
+    }, {
+      "pid": "3",
+      "image":"assets/sm_windows.jpeg",
+      "description": "Windows Phone",
+      "price": 110.00,
+      "quantity": 2
+    }]
+  }
+
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  addItem(item: string){
+    switch(item) {
+      case 'Android':
+      this.orders.unshift({
+        "pid": "1",
+        "image":"assets/sm_android.jpeg",
+        "description": "Android",
+        "price": 150.00,
+        "quantity": 1
+      });
+      break;
+
+      case 'IPhone':
+      this.orders.unshift({
+        "pid": "2",
+        "image":"assets/sm_iphone.jpeg",
+        "description": "IPhone",
+        "price": 200.00,
+        "quantity": 1
+      });
+      break;
+
+      case 'Windows Phone':
+      this.orders.unshift({
+        "pid": "3",
+        "image":"assets/sm_windows.jpeg",
+        "description": "Windows Phone",
+        "price": 110.00,
+        "quantity": 1
+      });
+      break;
+
+    }
+  }
+
+  clear(){
+    this.orders.map((item:IOrder, i: number) => {
+      Object.keys(item).map((key: string) => {
+      if(key != 'image'){
+        item[key]='';
+      } 
+      return item;
+    });
+    });
+  }
+
+  calculateTotal(){
+    const total = this.orders.reduce((acc: number, item: IOrder) => {
+      acc +=item.quantity * item.price;
+      return acc
+    }, 0);
+    const taxAmount = total * .1;
+    const subTotal = total - taxAmount;
+    const validated = this.validate(name, total, taxAmount, subTotal)
+    if (!validated) {
+      this.showMessage('error-Modal');
+    } else {
+      this.confirmMessage = this.setSuccessMessage(this.name, total, taxAmount, subTotal)
+      this.showMessage('confirm-modal')
+    }
+  }
+
+  showMessage(modalID: string){
+    this.flexModal.openDialog(modalID)
+  }
+
+  setSuccessMessage(name: string, total: number, taxAmount: number, subTotal: number){
+      const commaIndex = name.indexOf(', ');
+      const firstName = name.slice(commaIndex, name.length);
+      const lastName = name.slice(0, commaIndex);
+      let output = `Thank you for your order ${firstName} ${lastName}. Your subtotal is: ${subTotal}, Your tax amount is: ${taxAmount}, and your end total is ${total}`;
+      return output;
 
   }
 
